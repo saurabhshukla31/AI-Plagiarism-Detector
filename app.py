a67bb421470cf4922947cd12d8e2a48d59076f37
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-09-16 15:24:18 +0530

    .

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from nltk.corpus import stopwords
 import string
 
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
